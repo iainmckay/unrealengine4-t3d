8feeb3b424a14a08447802350df2b85c92b4b7b8
Iain Mckay <me@iainmckay.co.uk>
2020-08-04 20:31:39 +0200

    Added properties for Fresnel node

--- Material/MaterialExpressionFresnel.cs
+++ Material/MaterialExpressionFresnel.cs
@@ -5,8 +5,15 @@ namespace JollySamurai.UnrealEngine4.T3D.Material
 {
     public class MaterialExpressionFresnel : Node
     {
-        public MaterialExpressionFresnel(string name, int editorX, int editorY) : base(name, editorX, editorY)
+        public ParsedPropertyBag BaseReflectFractionIn { get; }
+        public ParsedPropertyBag ExponentIn { get; }
+        public ParsedPropertyBag Power { get; }
+
+        public MaterialExpressionFresnel(string name, ParsedPropertyBag baseReflectFractionIn, ParsedPropertyBag exponentIn, ParsedPropertyBag power, int editorX, int editorY) : base(name, editorX, editorY)
         {
+            BaseReflectFractionIn = baseReflectFractionIn;
+            ExponentIn = exponentIn;
+            Power = power;
         }
     }
 
@@ -18,8 +25,11 @@ namespace JollySamurai.UnrealEngine4.T3D.Material
         {
             AddRequiredAttribute("Name", PropertyDataType.String);
 
+            AddOptionalProperty("BaseReflectFractionIn", PropertyDataType.AttributeList);
+            AddOptionalProperty("ExponentIn", PropertyDataType.AttributeList);
             AddOptionalProperty("MaterialExpressionEditorX", PropertyDataType.Integer);
             AddOptionalProperty("MaterialExpressionEditorY", PropertyDataType.Integer);
+            AddOptionalProperty("Power", PropertyDataType.AttributeList);
 
             AddIgnoredProperty("Material");
             AddIgnoredProperty("MaterialExpressionGuid");
@@ -27,7 +37,14 @@ namespace JollySamurai.UnrealEngine4.T3D.Material
 
         public override Node Convert(ParsedNode node, Node[] children)
         {
-            return new MaterialExpressionFresnel(node.FindAttributeValue("Name"), ValueUtil.ParseInteger(node.FindPropertyValue("MaterialExpressionEditorX") ?? "0"), ValueUtil.ParseInteger(node.FindPropertyValue("MaterialExpressionEditorY") ?? "0"));
+            return new MaterialExpressionFresnel(
+                node.FindAttributeValue("Name"), 
+                ValueUtil.ParseAttributeList(node.FindPropertyValue("BaseReflectFractionIn") ?? null),
+                ValueUtil.ParseAttributeList(node.FindPropertyValue("ExponentIn") ?? null),
+                ValueUtil.ParseAttributeList(node.FindPropertyValue("Power") ?? null),
+                ValueUtil.ParseInteger(node.FindPropertyValue("MaterialExpressionEditorX") ?? "0"), 
+                ValueUtil.ParseInteger(node.FindPropertyValue("MaterialExpressionEditorY") ?? "0")
+            );
         }
     }
 }
