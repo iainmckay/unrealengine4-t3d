3deeb494ef0a2e2ec0b7747ca90ba63f0cbbe950
Iain Mckay <me@iainmckay.co.uk>
2020-08-06 22:08:02 +0200

    Fixed up missing fallback values and normal on Fresnel node

--- Material/MaterialExpressionFresnel.cs
+++ Material/MaterialExpressionFresnel.cs
@@ -6,14 +6,20 @@ namespace JollySamurai.UnrealEngine4.T3D.Material
     public class MaterialExpressionFresnel : Node
     {
         public ParsedPropertyBag BaseReflectFractionIn { get; }
+        public float BaseReflectFraction { get; }
         public ParsedPropertyBag ExponentIn { get; }
+        public float Exponent { get; }
         public ParsedPropertyBag Power { get; }
+        public ParsedPropertyBag Normal { get; }
 
-        public MaterialExpressionFresnel(string name, ParsedPropertyBag baseReflectFractionIn, ParsedPropertyBag exponentIn, ParsedPropertyBag power, int editorX, int editorY) : base(name, editorX, editorY)
+        public MaterialExpressionFresnel(string name, ParsedPropertyBag baseReflectFractionIn, float baseReflectFraction, ParsedPropertyBag exponentIn, float exponent, ParsedPropertyBag power, ParsedPropertyBag normal, int editorX, int editorY) : base(name, editorX, editorY)
         {
             BaseReflectFractionIn = baseReflectFractionIn;
+            BaseReflectFraction = baseReflectFraction;
             ExponentIn = exponentIn;
+            Exponent = exponent;
             Power = power;
+            Normal = normal;
         }
     }
 
@@ -25,10 +31,13 @@ namespace JollySamurai.UnrealEngine4.T3D.Material
         {
             AddRequiredAttribute("Name", PropertyDataType.String);
 
+            AddOptionalProperty("BaseReflectFraction", PropertyDataType.Float);
             AddOptionalProperty("BaseReflectFractionIn", PropertyDataType.AttributeList);
+            AddOptionalProperty("Exponent", PropertyDataType.Float);
             AddOptionalProperty("ExponentIn", PropertyDataType.AttributeList);
             AddOptionalProperty("MaterialExpressionEditorX", PropertyDataType.Integer);
             AddOptionalProperty("MaterialExpressionEditorY", PropertyDataType.Integer);
+            AddOptionalProperty("Normal", PropertyDataType.AttributeList);
             AddOptionalProperty("Power", PropertyDataType.AttributeList);
 
             AddIgnoredProperty("Material");
@@ -40,8 +49,11 @@ namespace JollySamurai.UnrealEngine4.T3D.Material
             return new MaterialExpressionFresnel(
                 node.FindAttributeValue("Name"), 
                 ValueUtil.ParseAttributeList(node.FindPropertyValue("BaseReflectFractionIn") ?? null),
+                ValueUtil.ParseFloat(node.FindPropertyValue("BaseReflectFraction") ?? "0.04"),
                 ValueUtil.ParseAttributeList(node.FindPropertyValue("ExponentIn") ?? null),
+                ValueUtil.ParseFloat(node.FindPropertyValue("Exponent") ?? "5.0"),
                 ValueUtil.ParseAttributeList(node.FindPropertyValue("Power") ?? null),
+                ValueUtil.ParseAttributeList(node.FindPropertyValue("Normal") ?? null),
                 ValueUtil.ParseInteger(node.FindPropertyValue("MaterialExpressionEditorX") ?? "0"), 
                 ValueUtil.ParseInteger(node.FindPropertyValue("MaterialExpressionEditorY") ?? "0")
             );
