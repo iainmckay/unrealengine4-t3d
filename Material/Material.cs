3fa581a8797a80553f68ea7cb7d3b542ade68299
Iain Mckay <me@iainmckay.co.uk>
2020-08-06 21:58:56 +0200

    Added OpacityMask to Material

--- Material/Material.cs
+++ Material/Material.cs
@@ -18,10 +18,11 @@
         public ParsedPropertyBag Specular { get; }
         public ParsedPropertyBag EmissiveColor { get; }
         public ParsedPropertyBag Opacity { get; }
+        public ExpressionReference OpacityMask { get; }
         public ExpressionReference[] Expressions { get; }
         public ExpressionReference[] EditorComments { get; }
 
-        public Material(Node[] children, string name, ShadingModel shadingModel, BlendMode blendMode, MaterialDomain materialDomain, TranslucencyLightingMode translucencyLightingMode, bool isTwoSided, ParsedPropertyBag baseColor, ParsedPropertyBag metallic, ParsedPropertyBag normal, ParsedPropertyBag roughness, ParsedPropertyBag specular, ParsedPropertyBag emissiveColor, ParsedPropertyBag opacity, ExpressionReference[] expressionReferences, ExpressionReference[] editorComments, int editorX, int editorY)
+        public Material(Node[] children, string name, ShadingModel shadingModel, BlendMode blendMode, MaterialDomain materialDomain, TranslucencyLightingMode translucencyLightingMode, bool isTwoSided, ParsedPropertyBag baseColor, ParsedPropertyBag metallic, ParsedPropertyBag normal, ParsedPropertyBag roughness, ParsedPropertyBag specular, ParsedPropertyBag emissiveColor, ParsedPropertyBag opacity, ExpressionReference opacityMask, ExpressionReference[] expressionReferences, ExpressionReference[] editorComments, int editorX, int editorY)
             : base(name, editorX, editorY, children)
         {
             ShadingModel = shadingModel;
@@ -36,6 +37,7 @@
             Specular = specular;
             EmissiveColor = emissiveColor;
             Opacity = opacity;
+            OpacityMask = opacityMask;
             Expressions = expressionReferences;
             EditorComments = editorComments;
         }
@@ -72,6 +74,7 @@
             AddOptionalProperty("Metallic", PropertyDataType.AttributeList);
             AddOptionalProperty("Normal", PropertyDataType.AttributeList);
             AddOptionalProperty("Opacity", PropertyDataType.AttributeList);
+            AddOptionalProperty("OpacityMask", PropertyDataType.ExpressionReference);
             AddOptionalProperty("Roughness", PropertyDataType.AttributeList);
             AddOptionalProperty("ShadingModel", PropertyDataType.ShadingModel);
             AddOptionalProperty("Specular", PropertyDataType.AttributeList);
@@ -113,6 +116,7 @@
                 ValueUtil.ParseAttributeList(node.FindPropertyValue("Specular")),
                 ValueUtil.ParseAttributeList(node.FindPropertyValue("EmissiveColor")),
                 ValueUtil.ParseAttributeList(node.FindPropertyValue("Opacity")),
+                ValueUtil.ParseExpressionReference(node.FindPropertyValue("OpacityMask")),
                 ValueUtil.ParseExpressionReferenceArray(node.FindProperty("Expressions")?.Elements),
                 ValueUtil.ParseExpressionReferenceArray(node.FindProperty("EditorComments")?.Elements),
                 ValueUtil.ParseInteger(node.FindPropertyValue("EditorX") ?? "0"),
